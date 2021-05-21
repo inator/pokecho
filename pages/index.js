23e9463d275a92883d811511c14689216481764d
notato8 <ethanshaw104@gmail.com>
2021-05-20 18:49:43 -0700

    Functional component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import React from 'react';
+import React, { useState } from 'react';
 
 import { getGamesData } from "../lib/games";
 
@@ -12,49 +12,30 @@ export async function getStaticProps() {
     };
 }
 
-
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            game: this.props.gamesData[0].id,
-        };
-
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleChange(e) {
-        this.setState({ game: e.target.value });
-    }
-
-    render() {
-        return (
-            <div>
-                <Head>
-                    <title>Pokécho</title>
-                    <meta name="description" content="Pokémon search engine" />
-                    <link rel="icon" href="/favicon.ico" />
-                </Head>
-    
-    
-                <h1>Pokécho</h1>
-                <label for="games">Game: </label>
-                <select id="games" onChange={this.handleChange}>
-                    {this.props.gamesData.map(({ id, name }) => (
-                        <option value={id}>{name}</option>
-                    ))}
-                </select>
-                <br />
-                <label for="search">Search: </label>
-                <input type="text" id="search"></input>
-    
-                <div>{this.state.game}</div>
-            </div>
-        );
-    }
-}
-
-
 export default function Test({ allGamesData }) {
-    return <App gamesData={allGamesData} />
+    const [game, setGame] = useState(allGamesData[0].id);
+
+    return (
+        <div>
+            <Head>
+                <title>Pokécho</title>
+                <meta name="description" content="Pokémon search engine" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+
+            <h1>Pokécho</h1>
+            <label for="games">Game: </label>
+            <select id="games" onChange={e => setGame(e.target.value)}>
+                {allGamesData.map(({ id, name }) => (
+                    <option value={id}>{name}</option>
+                ))}
+            </select>
+            <br />
+            <label for="search">Search: </label>
+            <input type="text" id="search"></input>
+
+            <div>{game}</div>
+        </div>
+    );
 }
